e7d57c6ca388d321c14d603570f8f3f68815cf2e
verangade <verangades@gmail.com>
2021-10-31 21:32:50 +0800

    add contacts component

--- client-ui/src/components/Contacts.js
+++ client-ui/src/components/Contacts.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import SingleContact from './SingleContact';
+import AddContacts from './AddContacts';
+
 export default class Contacts extends Component{
     constructor(props){
         super(props);
@@ -8,12 +11,27 @@ export default class Contacts extends Component{
         };
     }
 
+    componentDidMount(){
+        fetch("http://localhost:8080/getAll")
+        .then(response => response.json())
+        .then(data => this.setState({contacts : data}));
+    }
+
     render(){
         return(
         <div>
-            <p>Hello</p>
+            <div className="row">
+                <AddContacts />
+            </div>
+            <div className="row">
+                {
+                    this.state.contacts.map( (item) => (<SingleContact key={item.id} item={item} />) )
+                }
+            </div>
+          
 
-        </div>);
+        </div>
+        );
     }
 
 }
