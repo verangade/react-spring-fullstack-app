35e4f1726335d694bb8c355e7188b94b2c399993
verangade <verangades@gmail.com>
2021-10-31 22:14:04 +0800

    correct class to className

--- client-ui/src/components/AddContacts.js
+++ client-ui/src/components/AddContacts.js
@@ -29,27 +29,27 @@ export default class AddContacts extends Component{
     render(){
         return(
 
-            <div class="row">
-            <form class="col s12" onSubmit={this.submitContact.bind(this)}>
-              <div class="row">
-                <div class="input-field col s6">
-                  <input placeholder="Placeholder" ref="first_name" type="text" class="validate"/>
+            <div className="row">
+            <form className="col s12" onSubmit={this.submitContact.bind(this)}>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input placeholder="Placeholder" ref="first_name" type="text" className="validate"/>
                   <label htmlFor="first_name">First Name</label>
                 </div>
-                <div class="input-field col s6">
-                  <input ref="last_name" type="text" class="validate"/>
+                <div className="input-field col s6">
+                  <input ref="last_name" type="text" className="validate"/>
                   <label htmlFor="last_name">Last Name</label>
                 </div>
               </div>         
            
-              <div class="row">
-                <div class="input-field col s12">
-                  <input ref="email" type="email" class="validate"/>
+              <div className="row">
+                <div className="input-field col s12">
+                  <input ref="email" type="email" className="validate"/>
                   <label htmlFor="email">Email</label>
                 </div>
               </div>
-              <div class="row">
-                    <button className="waves-effect waves-light" type="submit" name="action">Submit</button>
+              <div className="row">
+                    <button classNameName="waves-effect waves-light" type="submit" name="action">Submit</button>
               </div>
             </form>
           </div>
